449d3414f5b70c337f10b21f06760bf033394e4a
azberto <117331182+azberto@users.noreply.github.com>
2022-11-19 13:18:53 +0530

    [skip ci]

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch superior_X00TD-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+export SELINUX_IGNORE_NEVERALLOWS=true 
+#export TARGET_KERNEL_CLANG_VERSION=proton  
 m bacon -j$(nproc --all)
-#end2
+#end3
