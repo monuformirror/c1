0991cd0d019cfb989eabf36116ab7414eed2ea3b
azberto <117331182+azberto@users.noreply.github.com>
2022-11-21 22:37:50 +0530

    Bruh moment

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom 
 . build/envsetup.sh
-export lunch cherish_X00TD-userdebug
+lunch cherish_X00TD-userdebug
 export TZ=Asia/Kolkata
 brunch X00TD
 #end7
