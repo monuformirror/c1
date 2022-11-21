5c7a7ddca3ec72c60cd5df5d9810511349a8ede6
azberto <117331182+azberto@users.noreply.github.com>
2022-11-21 14:51:05 +0530

    Intial rice

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/god-goku/local-manifest.git --depth 1 -b sharma .repo/local_manifests
+git clone https://github.com/azberto/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_veux-userdebug
+lunch lineage_X00TD-userdebug
 export TZ=Asia/Kolkata
-brunch veux 
-#end7
+brunch X00TD
+#end
