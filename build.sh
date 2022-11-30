332ecbdefdbf90b28d96282fed2eff672719cb32
monuformirror <110083657+monuformirror@users.noreply.github.com>
2022-11-30 21:37:46 +0530

    [skip ci]

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/god-goku/local-manifest.git --depth 1 -b cherry .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j24
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
@@ -11,5 +11,4 @@ export BUILD_USER=Monu
 export BUILD_HOST=Loda
 export BUILD_USERNAME=Monu
 export BUILD_HOSTNAME=Loda
-make bacon -j31
-#end13
+make bacon -j8
