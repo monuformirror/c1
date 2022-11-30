2bd7a69b65a677c3f4b3d84feb61836f6f948d8b
Monu <EMILIA.WATERS@student.csn.edu>
2022-11-30 18:16:30 +0530

    Rice

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/god-goku/local-manifest.git --depth 1 -b sharma .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j24
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
@@ -11,5 +11,5 @@ export BUILD_USER=SharmaG
 export BUILD_HOST=Loda
 export BUILD_USERNAME=SharmaG
 export BUILD_HOSTNAME=Loda
-make bacon -j31
-#end13
+make bacon -j8
+
